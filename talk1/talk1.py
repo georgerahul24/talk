938f8544a1a9733ba33c1c72a45ff0531b15f1e7
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-01-09 14:50:51 +0530

    added proper documentation

--- talk1/talk1.py
+++ talk1/talk1.py
@@ -10,13 +10,16 @@ rate = engine.getProperty("rate")  # getting details of current speaking rate
 engine.setProperty("rate", 135)  # setting up new voice rate
 
 
-def talk(speak):
+def talk(speak)->None:
+    """A function for text to speech functionality"""
     engine.say(speak)
     engine.runAndWait()
     del speak
 
 
-def multithreadtalk(speak):
+def multithreadtalk(speak)->None:
+    """A function for multithreaded text to speech functionality"""
+    # Still in prototype
     # for multi threading else the runtime error of loop still running will come
     try:
         engine.endLoop()
