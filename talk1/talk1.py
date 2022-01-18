726989148cdfedd4a495ce4e8a8fe3d753d93db3
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-01-18 11:36:52 +0530

    minor tweaks

--- talk1/talk1.py
+++ talk1/talk1.py
@@ -4,8 +4,7 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")  # getting details of current voice
 # engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-engine.setProperty(
-    "voice", voices[1].id)  # changing index, changes voices. 1 for female
+engine.setProperty("voice", voices[1].id)  # changing index, changes voices. 1 for female
 rate = engine.getProperty("rate")  # getting details of current speaking rate
 engine.setProperty("rate", 135)  # setting up new voice rate
 
@@ -16,5 +15,4 @@ def talk(speak, var: None | bool = None) -> None:
         if var is not None: print(speak)
         engine.say(speak)
         engine.runAndWait()
-        del speak
     except: pass
